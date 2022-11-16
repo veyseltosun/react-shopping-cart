6df0250c2fd7a0c6829187b55970da6b8035a173
veyseltosun <postalarim1923@gmail.com>
2022-11-16 12:08:03 +0300

    Navbar component.

--- shopping-cart/src/context.js
+++ shopping-cart/src/context.js
@@ -13,5 +13,15 @@ const initialState = {
 }
 
 const AppProvider =({children}) =>{
-    
+    const [cart, setCart] = useState(cartItems)
+
+ return(
+    <AppContext.Provider value={{cart,}}>{children}</AppContext.Provider>
+ )
+
 }
+
+export const useGlobalContext = () =>{
+    return useContext(AppContext)
+}
+export {AppContext, AppProvider}
