48b275b2681e7961f98b08ebcfc37f83169d3675
veyseltosun <postalarim1923@gmail.com>
2022-11-17 17:33:24 +0300

    ıncrease props.

--- shopping-cart/src/context.js
+++ shopping-cart/src/context.js
@@ -21,9 +21,15 @@ const AppProvider =({children}) =>{
     const remove = (id) =>{
         dispatch ({type:"REMOVE", payload:id})
     }
+    const increase = (id) => {
+        dispatch({type: "INCREASE", payload: id})
+    }
+    const decrease = (id) => {
+        dispatch({type: "DECREASE", payload: id})
+    }
 
  return(
-    <AppContext.Provider value={{...state, clearCart, remove, }}>{children}</AppContext.Provider>
+    <AppContext.Provider value={{...state, clearCart, remove, increase, decrease, }}>{children}</AppContext.Provider>
  )
 
 }
